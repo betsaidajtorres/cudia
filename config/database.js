164719970a3beb434e05e0572526cd30f3830f6e
betsaida <betsaida.j.torres@gmail.com>
2022-04-11 10:16:11 +0200

    algunos cambios

--- config/database.js
+++ config/database.js
@@ -2,12 +2,12 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'ec2-99-80-170-190.eu-west-1.compute.amazonaws.com'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'd192tbn6vterqe'),
-      user: env('DATABASE_USERNAME', 'oyfuoesjmmumjx'),
-      password: env('DATABASE_PASSWORD', '437a77ce5c47d9a5af10cf7cf6b96b68594837781a66e093f0df3a9925ad6f4b'),
-      ssl: env.bool('DATABASE_SSL', true),
+      host: process.env.DATABASE_HOST,
+      port: process.env.DATABASE_PORT,
+      database: process.env.DATABASE_NAME,
+      user: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      ssl: { rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false) },
     },
   },
 });
